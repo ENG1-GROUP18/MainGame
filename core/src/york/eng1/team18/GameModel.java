ee5bd2a64bc739de1954dbf976191c14bbce0035
ra1286 <94614531+ra1286@users.noreply.github.com>
2021-12-12 15:44:16 +0000

    cleaning up code

--- core/src/york/eng1/team18/GameModel.java
+++ core/src/york/eng1/team18/GameModel.java
@@ -10,104 +10,80 @@ import york.eng1.team18.loader.BodyEditorLoader;
 import york.eng1.team18.views.MainScreen;
 
 public class GameModel {
+    public MainScreen parent;
     public World world;
-    private OrthographicCamera camera;
     private InputController controller;
-    private Box2DDebugRenderer debugRenderer;
+
 
     public Body playerShip;
     public Body lakeBody;
     public Body islandBox;
 
 
-    public GameModel(InputController cont){
+    public GameModel(MainScreen screen, InputController cont){
+        parent = screen;
         controller = cont;
         world = new World(new Vector2(0, 0), true);
-        //createIsland();
         createBoat();
         createLake();
     }
 
     public void logicStep(float delta){
         // Game logic goes here
-        // For forward/backward force
         float enginePower = 100;
         float x = (float)Math.sin(playerShip.getAngle() - Math.PI);
         float y = (float)Math.cos(playerShip.getAngle());
 
         float speed_dependant = 1- Math.abs((30  - (Math.abs(playerShip.getLinearVelocity().x) + Math.abs(playerShip.getLinearVelocity().y)))/30);
 
-
         if (controller.forward) {
             // Increase linear damping, apply forward force
             playerShip.applyForceToCenter(x * enginePower, y * enginePower,true);
-            playerShip.setLinearDamping(5f);
-        } else {
-            // Reduce Linear Damping
-            playerShip.setLinearDamping(2f);
         }
         if (controller.backward) {
             playerShip.applyForceToCenter(-x * 0.2f * enginePower, -y * 0.2f * enginePower,true);
         }
         if (controller.left && !controller.right) {
-            System.out.println(speed_dependant);
             playerShip.setAngularVelocity(1.5f + (speed_dependant * 1.5f));
             playerShip.setAngularDamping(4f - speed_dependant);
         } else if (controller.right && !controller.left) {
             playerShip.setAngularVelocity(-1.5f - (speed_dependant * 1.5f));
             playerShip.setAngularDamping(4f - speed_dependant);
         }
-//        else {
-//            playerShip.setAngularVelocity(0);
-//        }
+        else {
+            playerShip.setAngularVelocity(0);
+        }
         world.step(delta, 3, 3);
     }
 
     private void createBoat() {
         BodyDef bodyDef = new BodyDef();
         bodyDef.type = BodyDef.BodyType.DynamicBody;
-        //bodyDef.position.set(-10, -5);
+
         playerShip = world.createBody(bodyDef);
-        playerShip.setLinearDamping(2);
+        playerShip.setLinearDamping(1);
         playerShip.setAngularDamping(1.4f);
+
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(1f, 2f);
-        playerShip.setTransform(-40, -10, (float)(Math.PI * 1.5));
-        playerShip.createFixture(shape, 0.0f);
+        shape.setAsBox(parent.playerWidth/2, parent.playerHeight/2);
+
+        playerShip.setTransform(-150, -140, 0);
+        playerShip.createFixture(shape, 0);
         shape.dispose();
     }
 
     private void createLake() {
-        // Create loader
         BodyEditorLoader loader = new BodyEditorLoader(Gdx.files.internal("paths/UniLake.json"));
-        // Create BodyDef
+
         BodyDef bd = new BodyDef();
-        bd.position.set(-50 , -50);
+        bd.position.set(-parent.mapScale/2, (-parent.mapScale / (parent.mapAspectRatio))/2);
         bd.type = BodyDef.BodyType.StaticBody;
 
-        // Create FixtureDef
         FixtureDef fd = new FixtureDef();
-        fd.density = 1;
         fd.friction = 0.5f;
-        fd.restitution = 0.3f;
 
-        // Create Body
         lakeBody = world.createBody(bd);
 
-        // Create body fixture with loader
-        loader.attachFixture(lakeBody, "UniLake", fd, 200f);
-    }
-
-
-
-    private void createIsland() {
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyDef.BodyType.StaticBody;
-        bodyDef.position.set(20, -5);
-        islandBox = world.createBody(bodyDef);
-        PolygonShape shape = new PolygonShape();
-        shape.setAsBox(10f, 10f);
-        islandBox.createFixture(shape, 0.0f);
-        shape.dispose();
+        loader.attachFixture(lakeBody, "Lake", fd, parent.mapScale);
     }
 }
--- core/src/york/eng1/team18/views/MainScreen.java
+++ core/src/york/eng1/team18/views/MainScreen.java
@@ -26,18 +26,31 @@ import york.eng1.team18.Orchestrator;
 import york.eng1.team18.controller.InputController;
 
 public class MainScreen implements Screen {
+
+    // When true, camera is still and zoomed out, used to debug.
+    //----------------------------------
+    private boolean CAMERA_FOLLOWS = true;
+    private boolean BOX2D_WIREFRAME = false;
+    //----------------------------------
+
+    private static final int mapImageX = 1155;  // height of map image
+    private static final int mapImageY = 776;   // width of map image
+    public float playerHeight = 4f;             // player height in world units
+    public float playerWidth = 2f;              // player width in world units
+    public float mapScale = 500f;               // map width in world units
+    public float mapAspectRatio = 1.49f;        // Aspect ratio of image used for map
+    public float cameraZoom = 30;               // ExtendViewport minimum size in world units
+
+
     private Orchestrator parent;
 
     InputController controller;
     GameModel model;
 
     OrthographicCamera camera;
-    Vector2 camPos;
     ExtendViewport viewport;
 
-
     Sprite playerSprite;
-    Sprite islandSprite;
     Sprite testMapSprite;
     SpriteBatch batch;
     ShapeRenderer shapeRenderer;
@@ -45,24 +58,24 @@ public class MainScreen implements Screen {
 
     public MainScreen(Orchestrator orchestrator) {
         parent = orchestrator;
-
         controller = new InputController();
-        model = new GameModel(controller);
         Gdx.input.setInputProcessor(controller);
+        model = new GameModel(this, controller);
+
+        batch = new SpriteBatch();
+        debugRenderer = new Box2DDebugRenderer(BOX2D_WIREFRAME,BOX2D_WIREFRAME,BOX2D_WIREFRAME,BOX2D_WIREFRAME,BOX2D_WIREFRAME,BOX2D_WIREFRAME);
 
         camera = new OrthographicCamera(1, 1);
-        viewport = new ExtendViewport(100, 100, camera);
+        if (!CAMERA_FOLLOWS) {
+            cameraZoom = mapScale / 1.5f;
+        }
+        viewport = new ExtendViewport(cameraZoom, cameraZoom, camera);
 
-        debugRenderer = new Box2DDebugRenderer(true,true,false,true,true,true);
+        playerSprite = new Sprite(new Texture(Gdx.files.internal("images/boatSprite.png")));
+        playerSprite.setSize(playerWidth, playerHeight);
 
-        playerSprite = new Sprite(new Texture(Gdx.files.internal("images/rubber_duck.jpg")));
-        playerSprite.setSize(2, 4);
-        testMapSprite = new Sprite(new Texture(Gdx.files.internal("paths/UniLake.png")));
-        // 200 = image scale when created.
-        // 132 = 200 * image height / image width.
-        testMapSprite.setSize(200, 132);
-        //islandSprite = new Sprite(new Texture(Gdx.files.internal("images/island.jpg")));
-        batch = new SpriteBatch();
+        testMapSprite = new Sprite(new Texture(Gdx.files.internal("paths/UniLake.jpg")));
+        testMapSprite.setSize(mapScale, mapScale * (mapImageY / (float)mapImageX));
     }
 
     @Override
@@ -75,26 +88,12 @@ public class MainScreen implements Screen {
         model.logicStep(delta);
         ScreenUtils.clear(parent.assMan.waterCol);
 
-        camera.position.x = model.playerShip.getPosition().x;
-        camera.position.y = model.playerShip.getPosition().y;
+        // Update camera position
+        if (CAMERA_FOLLOWS) {
+            camera.position.x = model.playerShip.getPosition().x;
+            camera.position.y = model.playerShip.getPosition().y;
+        }
         camera.update();
-
-
-//        shapeRenderer.setProjectionMatrix(camera.combined);
-//        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
-//        shapeRenderer.setColor(parent.assMan.landCol2);
-//        shapeRenderer.rect(model.islandBox.getPosition().x - 10, model.islandBox.getPosition().y - 10, 20, 20);
-//        shapeRenderer.end();
-
-
-//        batch.setProjectionMatrix(camera.combined);
-//        batch.begin();
-//        islandSprite.setSize(20, 20);
-//        islandSprite.setPosition(model.islandBox.getPosition().x, model.islandBox.getPosition().y);
-//        islandSprite.translate(-10, -10);
-//        islandSprite.draw(batch);
-//        batch.end();
-
 
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
@@ -103,7 +102,7 @@ public class MainScreen implements Screen {
         playerSprite.setPosition(model.playerShip.getPosition().x, model.playerShip.getPosition().y);
         playerSprite.setOriginCenter();
         playerSprite.setRotation(model.playerShip.getAngle()*57.3f);
-        playerSprite.translate(-1, -2);
+        playerSprite.translate(-playerWidth/2, -playerHeight/2);
 
         testMapSprite.draw(batch);
         playerSprite.draw(batch);
