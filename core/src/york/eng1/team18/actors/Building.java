e538ee7f3ad9e9bfb9fac6a49d23b20e3ecf9aa8
Sam <62548353+sk1735@users.noreply.github.com>
2021-12-20 18:49:30 +0000

    Create Building.java

---
+++ core/src/york/eng1/team18/actors/Building.java
@@ -0,0 +1,30 @@
+package york.eng1.team18.actors;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+
+public class Building extends Image {
+
+    Group parent;
+
+    public Building(Group parent, String imagePath) {
+        super(new Texture(Gdx.files.internal(imagePath)));
+        this.parent = parent;
+        this.setSize(parent.getWidth(), parent.getHeight());
+    }
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        super.draw(batch, parentAlpha);
+    }
+}
