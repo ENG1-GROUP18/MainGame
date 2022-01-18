8a24b4ed0ce2ed2c305192bacde7877ef63dc991
Morgan <me839@york.ac.uk>
2022-01-18 16:48:17 +0000

    Correct Enememy Cannon Position and Hitbox

--- core/src/york/eng1/team18/actors/EnemyBase.java
+++ core/src/york/eng1/team18/actors/EnemyBase.java
@@ -61,7 +61,7 @@ public class EnemyBase extends Group {
         body = world.createBody(bodyDef);
         body.setTransform(pos_x, pos_y, (float)-Math.PI);
         PolygonShape shape = new PolygonShape();
-        shape.setAsBox(size_x/2, size_y/2);
+        shape.setAsBox(1, 1);
 
         FixtureDef fixtureDef = new FixtureDef();
         fixtureDef.shape = shape;
@@ -78,7 +78,7 @@ public class EnemyBase extends Group {
         shape.dispose();
 
         // Add components to player
-        this.addActor(new Cannon(false, player, this, this.getWidth()*2/5, this.getHeight()*3/4, 0, world, camera, stage, body,inpt));
+        this.addActor(new Cannon(false, player, this, this.getWidth()*0.5f, this.getHeight()*0.5f, 0, world, camera, stage, body,inpt));
 
         // For rotation around center
         this.setOrigin(this.getWidth()/2, this.getHeight()/2);
