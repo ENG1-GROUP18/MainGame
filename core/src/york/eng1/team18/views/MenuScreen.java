98d63e8d8cb184d0e7b77a0e48623232f14d15b3
Morgan <me839@york.ac.uk>
2022-01-28 13:31:27 +0000

    Toggle Fullscreen

--- core/src/york/eng1/team18/views/MenuScreen.java
+++ core/src/york/eng1/team18/views/MenuScreen.java
@@ -1,6 +1,7 @@
 package york.eng1.team18.views;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Cursor;
 import com.badlogic.gdx.graphics.GL20;
@@ -51,12 +52,18 @@ public class MenuScreen implements Screen {
         // Create button objects
         TextButton playBtn = new TextButton("PLAY", skin);
         TextButton leaderboardBtn = new TextButton("LEADERBOARD", skin);
+        final TextButton fullScrn = new TextButton("SET FULLSCREEN",skin);
+        if (Gdx.graphics.isFullscreen()){
+            fullScrn.setText("SET WINDOWED");
+        }
+
         TextButton quitBtn = new TextButton("QUIT", skin);
 
         // Add button objects to table
         table.add(playBtn).height(playBtn.getHeight() -20).row();
         table.add(leaderboardBtn).height(leaderboardBtn.getHeight()-20).row();
-        table.add(quitBtn).height(quitBtn.getHeight()-20).row();
+        table.add(fullScrn).height(leaderboardBtn.getHeight()-20).row();
+        table.add(quitBtn).height(fullScrn.getHeight()-20).row();
 
 
         // PLAY BUTTON
@@ -91,6 +98,23 @@ public class MenuScreen implements Screen {
                 parent.changeScreen(Orchestrator.LEADERBOARD);
             }
         });
+
+        //TOGGLE FULLSCREEN
+        fullScrn.addListener((new ChangeListener() {
+            @Override
+            public void changed(ChangeEvent event, Actor actor) {
+                Boolean fullScreen = Gdx.graphics.isFullscreen();
+                Graphics.DisplayMode currentMode = Gdx.graphics.getDisplayMode();
+                if (fullScreen == true) {
+                    fullScrn.setText("SET FULLSCREEN");
+                    Gdx.graphics.setWindowedMode(1280, 720);
+
+                }else{
+                    fullScrn.setText("SET WINDOWED");
+                    Gdx.graphics.setFullscreenMode(currentMode);}
+
+            }
+        }));
 
         // QUIT BUTTON
         quitBtn.addListener(new ChangeListener() {
